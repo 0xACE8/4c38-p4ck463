d395fad0d08472498d7dd33b20759b0bed53eec5
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-09-03 05:09:03 -0700

    Update main.sh

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -40,6 +40,8 @@ git clone --depth 1 https://github.com/lucikap/luci-app-ua2f luciua2f && mv -n l
 #git clone --depth 1 https://github.com/rufengsuixing/luci-app-syncdial && sed -i 's/is online and tracking is active/is online/g' luci-app-syncdial/luasrc/model/cbi/syncdial.lua
 git clone --depth 1 https://github.com/tty228/luci-app-wechatpush
 git clone --depth 1 https://github.com/firkerword/openwrt-wrtbwmon wrtbwmon1 && mv -n wrtbwmon1/wrtbwmon  ./; rm -rf wrtbwmon1
+git clone --depth 1 https://github.com/awe1p/stun
+git clone --depth 1 https://github.com/muink/openwrt-go-stun
 
 #sed -i "/minisign:minisign/d" luci-app-dnscrypt-proxy2/Makefile
 sed -i 's/\(+luci-compat\)/\1 +luci-theme-argon/' luci-app-argon-config/Makefile
