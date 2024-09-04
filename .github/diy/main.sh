3fbd397a03eedaae215949413af3f99719155050
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-09-04 03:45:33 -0700

    Update main.sh

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -38,6 +38,7 @@ git clone --depth 1 https://github.com/muink/luci-app-homeproxy
 git clone --depth 1 https://github.com/Zxilly/UA2F openwrt-ua2f && mv -n openwrt-ua2f/openwrt ./ && mv openwrt ua2f; rm -rf openwrt-ua2f
 git clone --depth 1 https://github.com/lucikap/luci-app-ua2f luciua2f && mv -n luciua2f/luci-app-ua2f  ./; rm -rf luciua2f
 #git clone --depth 1 https://github.com/rufengsuixing/luci-app-syncdial && sed -i 's/is online and tracking is active/is online/g' luci-app-syncdial/luasrc/model/cbi/syncdial.lua
+git clone --depth 1 https://github.com/gaoyaxuan/luci-app-pushbot
 #git clone --depth 1 https://github.com/tty228/luci-app-wechatpush
 git clone --depth 1 https://github.com/firkerword/openwrt-wrtbwmon wrtbwmon1 && mv -n wrtbwmon1/wrtbwmon  ./; rm -rf wrtbwmon1
 git clone --depth 1 https://github.com/awe1p/stun
