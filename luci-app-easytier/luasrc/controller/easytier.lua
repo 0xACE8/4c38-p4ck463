1363e2b70ef15368cd2c6ae7c043ab2a93f86907
0xACE8 <0xACE8@users.noreply.github.com>
2024-09-19 06:29:29 -0700

    update 2024-09-19 06:29:29

--- luci-app-easytier/luasrc/controller/easytier.lua
+++ luci-app-easytier/luasrc/controller/easytier.lua
@@ -6,12 +6,12 @@ function index()
 		return
 	end
                   
-        entry({"admin", "vpn", "easytier"}, alias("admin", "vpn", "easytier", "easytier"),_("EasyTier"), 46).dependent = true
-	entry({"admin", "vpn", "easytier", "easytier"}, cbi("easytier"),_("EasyTier"), 47).leaf = true
-	entry({"admin", "vpn",  "easytier",  "easytier_log"}, form("easytier_log"),_("日志"), 48).leaf = true
-	entry({"admin", "vpn", "easytier", "get_log"}, call("get_log")).leaf = true
-	entry({"admin", "vpn", "easytier", "clear_log"}, call("clear_log")).leaf = true
-	entry({"admin", "vpn", "easytier", "status"}, call("act_status")).leaf = true
+        entry({"admin", "services", "easytier"}, alias("admin", "services", "easytier", "easytier"),_("EasyTier"), 90).dependent = true
+	entry({"admin", "services", "easytier", "easytier"}, cbi("easytier"),_("EasyTier"), 47).leaf = true
+	entry({"admin", "services",  "easytier",  "easytier_log"}, form("easytier_log"),_("日志"), 48).leaf = true
+	entry({"admin", "services", "easytier", "get_log"}, call("get_log")).leaf = true
+	entry({"admin", "services", "easytier", "clear_log"}, call("clear_log")).leaf = true
+	entry({"admin", "services", "easytier", "status"}, call("act_status")).leaf = true
 end
 
 function act_status()
