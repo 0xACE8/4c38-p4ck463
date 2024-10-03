80746351439f8906161f675a193a86f58cd6c11f
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-10-03 01:49:53 -0700

    Update main.sh

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -79,19 +79,30 @@ git clone --depth 1 https://github.com/awe1p/stun
 #git clone --depth 1 https://github.com/muink/openwrt-go-stun
 
 # patch
+sed -i "s/nil, 0)/nil, -1)/g" luci-app-passwall2/luasrc/controller/passwall2.lua
+
 sed -i 's\../../\$(TOPDIR)/feeds/packages/\g' dnscrypt-proxy2/Makefile
+sed -i 's/START=18/START=98/g' dnscrypt-proxy2/files/dnscrypt-proxy.init
+#sed -i "/minisign:minisign/d" luci-app-dnscrypt-proxy2/Makefile
+
+sed -i 's/START=75/START=99/g' luci-app-mosdns/root/etc/init.d/mosdns
+
+sed -i 's/LUCI_DEPENDS:=/LUCI_DEPENDS:=+kmod-tun +luci-lib-fs/g' luci-app-easytier/Makefile
 #sed -i 's/vpn/services/g' luci-app-easytier/luasrc/controller/easytier.lua
 #sed -i 's/, 46)/, 90)/g' luci-app-easytier/luasrc/controller/easytier.lua
-sed -i 's/LUCI_DEPENDS:=/LUCI_DEPENDS:=+kmod-tun +luci-lib-fs/g' luci-app-easytier/Makefile
+
 #sed -i 's/微信推送/TG Push/g' luci-app-wechatpush/po/zh_Hans/wechatpush.po
 #sed -i '4d' luci-app-wechatpush/root/usr/share/luci/menu.d/luci-app-wechatpush.json
 #sed -i '4 i\\t\t"order": 60,' luci-app-wechatpush/root/usr/share/luci/menu.d/luci-app-wechatpush.json
+
 sed -i 's/, 30)/, 60)/g' luci-theme-serverchan/luasrc/controller/serverchan.lua
-#sed -i "/minisign:minisign/d" luci-app-dnscrypt-proxy2/Makefile
+
+
 sed -i 's/\(+luci-compat\)/\1 +luci-theme-argon/' luci-app-argon-config/Makefile
 sed -i "s/), 0)/), -1)/g" luci-app-passwall2/luasrc/controller/passwall2.lua
-sed -i "s/nil, 0)/nil, -1)/g" luci-app-passwall2/luasrc/controller/passwall2.lua
 #sed -i 's/\(+luci-compat\)/\1 +luci-theme-design/' luci-app-design-config/Makefile
 #sed -i 's/\(+luci-compat\)/\1 +luci-theme-argone/' luci-app-argone-config/Makefile
+
+
 
 exit 0
